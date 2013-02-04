bd52faee38bab587245d7b6dd03b64d83e6e146a
Shekhar Gulati <shekhargulati84@gmail.com>
2013-02-04 11:34:04 +0530

    @WebFilter added

---
+++ src/main/java/com/tomcat7/servlets/LoggingFilter.java
@@ -0,0 +1,29 @@
+package com.tomcat7.servlets;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/sayhello")
+public class LoggingFilter implements Filter {
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		System.out.println("Host name " + request.getRemoteHost());
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
--- src/main/java/com/tomcat7/servlets/SayHelloToOpenShiftServlet.java
+++ src/main/java/com/tomcat7/servlets/SayHelloToOpenShiftServlet.java
@@ -1,22 +1,22 @@
 package com.tomcat7.servlets;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorldServlet
- */
-@WebServlet("/sayhello")
+@WebServlet(value = "/sayhello", loadOnStartup = 1)
 public class SayHelloToOpenShiftServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().println("<h1>Hello from OpenShift !!!</h1>");
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println(
+				"<h1>Hello from OpenShift at " + new Date() + " !!!</h1>");
 	}
 
-	
 }
